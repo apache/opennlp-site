176e3979407c843c86ef619262b2a3ac476de972
buildbot <users@infra.apache.org>
2024-04-25 08:15:44 +0000

    Automatic Site Publish by Buildbot

---
+++ docs/2.3.3/apidocs/opennlp-morfologik-addon/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"opennlp.morfologik.builder"},{"l":"opennlp.morfologik.cmdline"},{"l":"opennlp.morfologik.cmdline.builder"},{"l":"opennlp.morfologik.lemmatizer"},{"l":"opennlp.morfologik.tagdict"},{"l":"opennlp.morfologik.util"}];updateSearchResults();
